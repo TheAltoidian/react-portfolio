f2efd2c17498d9f26535e3489b29ca811fe5f732
Cullen <cullenjcasey@gmail.com>
2022-06-07 20:23:17 -0700

    added game

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,17 +2,18 @@ import React from "react";
 import catmap from "../../assets/images/catmap.png";
 import budgetTracker from "../../assets/images/budgetTracker.png";
 import teamMember from "../../assets/images/teamMember.png";
+import rpgGame from "../../assets/images/rpgGame.png";
 
 function Portfolio() {
 
     return (
         <section id="Portfolio">
             <div>
-                <img src={catmap} style={{ width: "25%" }} alt="Cats to Maps" />
+                <img src={rpgGame} style={{ width: "25%" }} alt="Frozen Falls" />
                 <br></br>
-                <a href="https://github.com/TheAltoidian/Cats-to-Maps" target="_blank">Cats to Maps Githup</a>
+                <a href="https://github.com/smendoza94/rpg-game" target="_blank">Frozen Falls Githup</a>
                 <br></br>
-                <a href="https://thealtoidian.github.io/Cats-to-Maps/" target="_blank">Cats to Maps deployed</a>
+                <a href="https://frozen-falls-81566.herokuapp.com/" target="_blank">Frozen Falls deployed</a>
             </div>
             <div >
                 <img src={budgetTracker} style={{ width: "25%" }} alt="Budget Tracker" />
@@ -21,6 +22,13 @@ function Portfolio() {
                 <br></br>
                 <a href="https://powerful-earth-63050.herokuapp.com/" target="_blank">Budget Tracker deployed</a>
             </div>
+            <div>
+                <img src={catmap} style={{ width: "25%" }} alt="Cats to Maps" />
+                <br></br>
+                <a href="https://github.com/TheAltoidian/Cats-to-Maps" target="_blank">Cats to Maps Githup</a>
+                <br></br>
+                <a href="https://thealtoidian.github.io/Cats-to-Maps/" target="_blank">Cats to Maps deployed</a>
+            </div>
             <div className="felx-row">
                 <img src={teamMember} style={{ width: "25%" }} alt="Team Member Management" />
                 <br></br>
@@ -28,11 +36,6 @@ function Portfolio() {
                 <br></br>
                 <a href="https://cullen-team-member-manager.herokuapp.com/" target="_blank">Team Member Management deployed</a>
             </div>
-            {/* <div>
-                <img src={catmap} style={{ width: "25%" }} alt="Cats to Maps" />
-                <a href="https://github.com/TheAltoidian/Cats-to-Maps" target="_blank">Cats to Maps Githup</a>
-                <a href="https://thealtoidian.github.io/Cats-to-Maps/" target="_blank">Cats to Maps deployed</a>
-            </div> */}
         </section>
     );
 }
